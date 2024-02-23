0b401b661360f72a023d3355b4458dd029e8d3db
Miras <miras@gmail.com>
2024-02-23 20:40:41 +0600

    feat: add simple websocket server

--- index.ts
+++ index.ts
@@ -1,6 +1,18 @@
-import { httpServer } from "./src/http_server/index.js";
+import { httpServer } from './src/http_server/index.js'
 
-const HTTP_PORT = 8181;
+const HTTP_PORT = 8181
 
-console.log(`Start static http server on the ${HTTP_PORT} port!`);
-httpServer.listen(HTTP_PORT);
+console.log(`Start static http server on the ${HTTP_PORT} port!`)
+httpServer.listen(HTTP_PORT)
+
+import { WebSocketServer } from 'ws'
+const wss = new WebSocketServer({ port: 8080 }, () => console.log('WS server is listening at ws://localhost:8080'))
+
+wss.on('connection', ws => {
+	ws.on('message', message => {
+		console.log('received: %s', message)
+
+		ws.send('something')
+	})
+	ws.on('error', console.error)
+})
